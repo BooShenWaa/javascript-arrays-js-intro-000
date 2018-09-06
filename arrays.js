736908e6e4dc73879e143c13cc86d83a1c5b7563
Aaron Bougouneau <a.bougouneau@gmail.com>
2018-09-06 07:52:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,11 +6,21 @@ var chocolateBars = [
 ]
 
 function addElementToBeginningOfArray(array, element) {
-  const newArray = [element, ...array]
+  const newArray = [element, ...array];
   return newArray
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-    array.unshift(element)
+    array.unshift(element);
+    return array
+}
+
+function addElementToEndOfArray(array, element) {
+  const newArray = [...array, element];
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+    array.push(element);
     return array
 }
