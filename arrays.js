5a37a1ad00a02479e0f4e1b4b4377719d9a4636f
Aaron Bougouneau <a.bougouneau@gmail.com>
2018-09-06 07:12:48 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
+function addElementToBeginningOfArray(array, element) {
+  const newArray = [element, ...array]
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift("element")
+}
